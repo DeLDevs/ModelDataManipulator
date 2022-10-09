bcf36315134a76bec5b5cedc4bf258aa75592739
DaltonJabberwo <71056017+DaltonJabberwo@users.noreply.github.com>
2022-10-08 19:43:33 -0600

    Added documentation
    Added documentation comments in fun.DeLson.ModelDataManipulator.Utils.Utils class.

--- src/fun/DeLson/ModelDataManipulator/Utils/Utils.java
+++ src/fun/DeLson/ModelDataManipulator/Utils/Utils.java
@@ -2,8 +2,17 @@ package fun.DeLson.ModelDataManipulator.Utils;
 
 import org.bukkit.ChatColor;
 
+/**
+ * The Utils class adds utilities used by the plugin.
+ */
+
 public class Utils {
 
+	/**
+	 * Applies color codes to text.
+	 * @param s String of text to apply color codes to
+	 * @return Inputted string with color codes applied.
+	 */
 	public static String chat(String s) {
 		return ChatColor.translateAlternateColorCodes('&', s);
 	}
